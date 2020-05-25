95a57d8c29fdfeeaa168ffef6063542128d48199
Toni <tonizeno5@gmail.com>
2020-05-25 17:20:56 +0200

    Add Favicon and Update Title

---
+++ src/Banner/Banner.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const Banner = styled.div`
+  height: 325px;
+  background-image: url(img/banner5.jpg);
+  background-position: top;
+  background-size: cover;
+  filter: contrast(90%);
+`
